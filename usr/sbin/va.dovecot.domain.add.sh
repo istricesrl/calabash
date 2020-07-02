17a2e2f2e08339a3a97c60d9b9b9b3055c30dcde
Fabio Mosti <fabio.mosti@gmail.com>
2020-07-02 18:14:34 +0200

    aggiornamento script del gio  2 lug 2020, 18.14.34, CEST

--- usr/sbin/va.dovecot.domain.add.sh
+++ usr/sbin/va.dovecot.domain.add.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-# aggiunta dominio
+# se
+if [ -n "$1" ]; then
+
+    # aggiunta dominio
+    mysql --defaults-file=/etc/mysql.conf -u root -e "INSERT INTO mailserver.virtual_domains ( id, name ) VALUES ( NULL, '$1');"
+
+else
+
+    echo "$0 dominio"
+
+fi
 
 # REVISIONI
 # 2020-07-02 controllo funzionamento su Debian 10 (buster)

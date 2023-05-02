d4fe6f33d0eb10c696cbc2394d7ad65bedd7a4ca
Fabio Mosti <fabio.mosti@gmail.com>
2023-05-02 20:02:33 +0200

    aggiornamento script del mar  2 mag 2023, 20.02.32, CEST

---
+++ usr/sbin/va.rkhunter.setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+apt-get install rkhunter
+
+cp -r /usr/share/doc/va.script/examples/etc/rkhunter.conf /etc/
+cp -r /usr/share/doc/va.script/examples/etc/default/rkhunter /etc/default/
